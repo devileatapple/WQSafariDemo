76ad2c4e09c895ca870c0e465eea5a135688f3c8
陈伟强 <348112447@qq.com>
2019-03-20 14:52:18 +0800

    添加推荐使用粘贴板获取渠道信息的注释

--- WQSafariDemo/ViewController.m
+++ WQSafariDemo/ViewController.m
@@ -17,8 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    [self performSelector:@selector(getChannel) withObject:nil afterDelay:2.0f];
+    [self performSelector:@selector(getChannel) withObject:nil afterDelay:2.0f];//延时调用获取渠道信息
 }
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
@@ -27,16 +26,15 @@
     [super viewDidAppear:animated];
 }
 #pragma mark - 渠道获取
--(void)getChannel{
-    
-    //    NSString *url=@"http://static.thextrend.com/activity/hwtg_download/index.html";
-    NSString *url=@"http://static.thextrend.com/activity/blank/index.html";//空白页
-    
+-(void)getChannel {
+    NSString *url=@"http://static.thextrend.com/activity/blank/index.html";//在cookie中包含渠道信息的链接
     self.safariView = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:url]];
     self.safariView.delegate=self;
-    
     [self presentViewController:self.safariView animated:NO completion:nil];
+    
+    //鉴于SFSafariViewController的不足（会有授权弹窗，低版本可能获取不到safari 的cookie信息），推荐使用粘贴板来实现获取渠道信息。思路是整个iOS系统中共用了粘贴板，所以在渠道链接中给粘贴板赋值，在此获取系统粘贴板中的渠道信息。这里不详谈。
 }
+#pragma mark - SFSafariViewControllerDelegate
 -(void)safariViewControllerDidFinish:(SFSafariViewController*)controller {
     NSLog(@"safariViewControllerDidFinish===%s",__func__);
 }
